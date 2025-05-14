f504b46a16bffb828bc0e0ee2059188989b00b43
Jay <jaydwivedi.programmer@gmail.com>
2025-05-14 15:22:53 -0400

    Session 3 Admin Models ORM

--- E_COM/Auth_App/admin.py
+++ E_COM/Auth_App/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from . models import Contact,Test
 # Register your models here.
+
+# admin.site.register(Contact)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display=['id','name','phone','message']
+    search_fields=['name','phone','message']
+    list_filter=['name','id']
+
+admin.site.register(Test)
+
--- E_COM/Auth_App/models.py
+++ E_COM/Auth_App/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=120)
+    phone=models.CharField(max_length=10)
+    message=models.TextField(max_length=200)
+
+class Test(models.Model):
+    email=models.EmailField(max_length=100, primary_key=True)
+    fullname=models.CharField(max_length=120)
+    phone=models.CharField(max_length=10)
+    message=models.TextField(max_length=200)
+
+    
